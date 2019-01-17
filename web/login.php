9b601a9a01fd5d79af8a4951ab080d015728e7d6
MikeyG <michaeltgarrard@gmail.com>
2019-01-17 10:33:37 -0700

    added extra navbars

---
+++ web/login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <header></header>
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+</style>
+</head>
+
+<body>
+
+<ul>
+  <li><a href="/home.php">Home</a></li>
+  <li><a href="/about-us.php">About Us</a></li>
+  <li><a class="active" href="#login">Login</a></li>
+</ul>
+
+</body>
+
+</html>
