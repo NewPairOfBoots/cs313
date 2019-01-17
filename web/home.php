afefc2f791ffe3030bf1006db56ff0a45fb08c85
MikeyG <michaeltgarrard@gmail.com>
2019-01-17 10:28:49 -0700

    added navbar

--- web/home.php
+++ web/home.php
@@ -3,10 +3,40 @@
 <head>
 	<header></header>
 <style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
 </style>
 </head>
 
 <body>
+
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="/about-us.php">About Us</a></li>
+  <li><a href="/login.php">Login</a></li>
+</ul>
+
 	<h1>
 		Welcome. You are not logged in.
 	</h1>
