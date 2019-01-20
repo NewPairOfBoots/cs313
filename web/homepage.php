6c54e99b96c7c0e5ae9b82cc0ed5e6d9e56ab992
MikeyG <michaeltgarrard@gmail.com>
2019-01-19 21:38:32 -0700

    messing with gradients

--- web/homepage.php
+++ web/homepage.php
@@ -8,7 +8,7 @@
 	<?php include("includes/navigation.html"); ?>
 
 	<h1>Intro</h1>
-	<div class="maintext"><p>My name is Michael Garrard. I'm a Software Engineering student at BYU-Idaho. I enjoy music, games, anime, and spending time with my friends and family. Below is a picture of me (I'm second from the right) with my mom, dad, brother, and sister-in-law.</p>
+	<div class="maintext"><p>My name is Michael Garrard. I'm a Software Engineering student at BYU-Idaho. I enjoy music, games, anime, and spending time with my friends and family. I served a 2-year proselyting mission in Michigan, and loved it. I have one brother, one sister, and about 15 guitars. Below is a picture of me (I'm second from the right) with my mom, dad, brother, and sister-in-law.</p>
 	</div>
 
 	<img src="images/family2015.jpg" alt="familyphoto" class="responsive">
