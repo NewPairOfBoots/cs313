eabca1b3e5277be5f3d64ce2d0ab130c61bd67ce
MikeyG <michaeltgarrard@gmail.com>
2019-01-19 20:59:47 -0700

    changed font styles

--- web/homepage.php
+++ web/homepage.php
@@ -8,7 +8,8 @@
 	<?php include("includes/navigation.html"); ?>
 
 	<h1>Intro</h1>
-	<p>My name is Michael Garrard. I'm a Software Engineering student at BYU-Idaho. I enjoy music, games, anime, and spending time with my friends and family. Below is a picture of me (I'm second from the right) with my mom, dad, brother, and sister-in-law.</p>
+	<div class="maintext"><p>My name is Michael Garrard. I'm a Software Engineering student at BYU-Idaho. I enjoy music, games, anime, and spending time with my friends and family. Below is a picture of me (I'm second from the right) with my mom, dad, brother, and sister-in-law.</p>
+	</div>
 
 	<img src="images/family2015.jpg" alt="familyphoto" class="responsive">
 
