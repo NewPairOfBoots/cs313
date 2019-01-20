905aa79700f5f244c5bd07d0dcd95a2ddb2ef358
MikeyG <michaeltgarrard@gmail.com>
2019-01-19 21:24:23 -0700

    changed link location for music to image

--- web/mymusic.php
+++ web/mymusic.php
@@ -7,10 +7,10 @@
 
 	<?php include("includes/navigation.html"); ?>
 
-	<div class="maintext"><p>My biggest passion is composing music. Specifically rock music. I'd personally love to make a career out of music, but if that doesn't allow me to support a family, I would like to work a software job. Which is why I'm studying software engineering. I do feel that I'd enjoy a coding job as well. <a href="https://soundcloud.com/michaelgarrard/dawn">Here</a> is a link to my most recently uploaded recording. I am in the process of writing lyrics, and will be implementing female vocals which will replace the lead melody that begins 22 seconds into the song.</p>
+	<div class="maintext"><p>My biggest passion is composing music. Specifically rock music. I'd personally love to make a career out of music, but if that doesn't allow me to support a family, I would like to work a software job. Which is why I'm studying software engineering. I do feel that I'd enjoy a coding job as well. Click the image below to be redirected to my most recently uploaded recording. I am in the process of writing lyrics, and will be implementing female vocals which will replace the lead melody that begins 22 seconds into the song.</p>
 	</div>
 
-	<img src="images/guitarcollection.jpg" alt="guitarcollection">
+	<a href="https://soundcloud.com/michaelgarrard/dawn"><img src="images/guitarcollection.jpg" alt="guitarcollection"></a>
 
 
 </body>
