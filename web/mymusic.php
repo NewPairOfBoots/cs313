28d426518dfff85288e86f1124ba34aeb2be8c66
MikeyG <michaeltgarrard@gmail.com>
2019-01-19 21:45:29 -0700

    added some more info about myself

--- web/mymusic.php
+++ web/mymusic.php
@@ -7,7 +7,9 @@
 
 	<?php include("includes/navigation.html"); ?>
 
-	<div class="maintext"><p>My biggest passion is composing music. Specifically rock music. I'd personally love to make a career out of music, but if that doesn't allow me to support a family, I would like to work a software job. Which is why I'm studying software engineering. I do feel that I'd enjoy a coding job as well. Click the image below to be redirected to my most recently uploaded recording. I am in the process of writing lyrics, and will be implementing female vocals which will replace the lead melody that begins 22 seconds into the song.</p>
+	<h1>My music background</h1>
+
+	<div class="maintext"><p>My biggest passion is composing music. Specifically rock music. I started playing guitar when I was about 13 years old, and have loved it ever since. My dad taught me a little at first, and his love for rock music definitely rubbed off on me. I grew up listening to the music albums he owned. Bands I remember were: Queen, The Eagles, Michael Jackson, Paul McCartney, The Doobie Brothers, etc. I'd personally love to make a career out of music, but if that doesn't allow me to support a family, I would like to work a software job. Which is why I'm studying software engineering. I do feel that I'd enjoy a coding job as well. Click the image below to be redirected to my most recently uploaded recording. I am in the process of writing lyrics, and will be implementing female vocals which will replace the lead melody that begins 22 seconds into the song.</p>
 	</div>
 
 	<a href="https://soundcloud.com/michaelgarrard/dawn"><img src="images/guitarcollection.jpg" class="clickable" alt="guitarcollection"></a>
